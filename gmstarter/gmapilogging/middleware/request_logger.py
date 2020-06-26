62ec93de3e69c3215ab0d61f0d09a0705340563f
SydeWynder <prasenjit0625@gmail.com>
2020-06-27 00:08:08 +0530

    1

--- gmstarter/gmapilogging/middleware/request_logger.py
+++ gmstarter/gmapilogging/middleware/request_logger.py
@@ -13,6 +13,7 @@ p = Producer(settings.KAFKA_CONFIG)
 p_key = settings.GM_SERVER_NAME
 
 print("hohoho")
+print("hehehe")
 
 
 class GMLoggerMiddleware(MiddlewareMixin):
@@ -26,8 +27,16 @@ class GMLoggerMiddleware(MiddlewareMixin):
         request._request_start_time = curr_time
         request._request_id = str(uuid.uuid4())
         # value = {'path' : str(request.path), 'time' : str(curr_time), 'request_id' : request._request_id, 'info' : self.get_request_information(request)}
-        value = {'type' : 'request', 'path' : str(request.path), 'time' : str(curr_time), 'request_id' : request._request_id, 'info' : self.get_request_information(request)}
-        p.produce('request_logging', key=p_key, value=json.dumps(value).encode('utf-8'), callback=None)
+
+        try:
+            value = {'type' : 'request', 'path' : str(request.path), 'time' : str(curr_time), 'request_id' : request._request_id, 'info' : self.get_request_information(request)}
+            p.produce('request_logging', key=p_key, value=json.dumps(value).encode('utf-8'), callback=None)
+        except Exception as e:
+            try:
+                value1 = {'type' : 'kafka_exception', 'path' : str(request.path), 'time' : str(curr_time), 'request_id' : request._request_id, 'info' : str(traceback.format_exc()), 'exception_type' : 'request'}
+                p.produce('request_logging', key=p_key, value=json.dumps(value1).encode('utf-8'), callback=None)
+            except Exception as e1:
+                print(str(traceback.format_exc()))
         # p.produce('request_logging', value="testiing11", callback=None)
         # self.audit_logger.info('Got Request for the API %s at time %s.Generated request id is %s. Request data is %s.\n',
         #                        request.path, curr_time, request._request_id, self.get_request_information(request))
@@ -39,10 +48,19 @@ class GMLoggerMiddleware(MiddlewareMixin):
             res_content = response.content.decode('UTF-8')
         except:
             res_content = response.content
-        value = {'type' : 'response', 'path' : str(request.path), 'time_diff' : str(diff.total_seconds()), 'request_id' : request._request_id, 
-            'info' : str(res_content), 'status' : response.status_code}
-        print(value)
-        p.produce('request_logging', key=p_key, value=json.dumps(value).encode('utf-8'), callback=None)
+        # print(value)
+        try:
+            value = {'type' : 'response', 'path' : str(request.path), 'time_diff' : str(diff.total_seconds()), 'request_id' : request._request_id, 
+                'info' : str(res_content), 'status' : response.status_code}
+            p.produce('request_logging', key=p_key, value=json.dumps(value).encode('utf-8'), callback=None)
+        except Exception as e:
+            try:
+                value1 = {'type' : 'kafka_exception', 'path' : str(request.path), 'time_diff' : str(diff.total_seconds()), 'request_id' : request._request_id, 
+                    'info' : str(traceback.format_exc()), 'status' : response.status_code, 'exception_type' : 'response'}
+                p.produce('request_logging', key=p_key, value=json.dumps(value1).encode('utf-8'), callback=None)
+            except Exception as e1:
+                print(str(traceback.format_exc()))
+            # print(e)
         # self.metric_logger.info('%s sec elapsed in processing API %s request with id %s. \nResponse %s \n', diff.total_seconds(),
         #                         request.path, request._request_id,res_content)
         return response
@@ -60,8 +78,14 @@ class GMLoggerMiddleware(MiddlewareMixin):
         diff = curr_time - request._request_start_time
         
         # print traceback.format_exc()
-        value = {'type' : 'exception', 'path' : str(request.path), 'time_diff' : str(diff.total_seconds()), 'request_id' : request._request_id, 
-            'info' : str(traceback.format_exc()), 'status' : 1000}
-        p.produce('request_logging', key=p_key, value=json.dumps(value).encode('utf-8'), callback=None)
-        
-        
+        try:
+            value = {'type' : 'exception', 'path' : str(request.path), 'time_diff' : str(diff.total_seconds()), 'request_id' : request._request_id, 
+                'info' : str(traceback.format_exc()), 'status' : 1000}
+            p.produce('request_logging', key=p_key, value=json.dumps(value).encode('utf-8'), callback=None)
+        except Exception as e:
+            try:
+                value1 = {'type' : 'kafka_exception', 'path' : str(request.path), 'time_diff' : str(diff.total_seconds()), 'request_id' : request._request_id, 
+                    'info' : str(traceback.format_exc()), 'status' : 1000, 'exception_type' : 'exception'}
+                p.produce('request_logging', key=p_key, value=json.dumps(value1).encode('utf-8'), callback=None)
+            except Exception as e1:
+                print(str(traceback.format_exc()))
