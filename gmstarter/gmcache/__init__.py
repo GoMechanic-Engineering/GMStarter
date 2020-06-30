4ec2d540ce3924848aad9f6ba72f55749824691a
SydeWynder <prasenjit0625@gmail.com>
2020-06-30 20:06:49 +0530

    redis

--- gmstarter/gmcache/__init__.py
+++ gmstarter/gmcache/__init__.py
@@ -3,6 +3,7 @@ import sys
 from django.db.models import Model
 from django.apps import apps
 import json
+import functools
 
 
 class GMCache:
@@ -35,3 +36,15 @@ class GMCache:
 
     def expireat(self, key, when):
         self.REDIS.expireat(key, when)
+
+    def setURLResponse(self, request, value=None, weeks=0, days=0, hours=0, minutes=0, seconds=0, nExists=False, xExists=False):
+        expiry = (weeks*7*24*60*60) + (days*24*60*60) + (hours*60*60) + (hours*60*60) + (minutes*60) + seconds
+        key = request.path.replace('/','-*-') + '-*-'.join([*list(functools.reduce(lambda x, y: x + y, sorted(request.query_params.items())))])
+        if expiry != 0:
+            self.REDIS.set(key, value, ex=expiry, nx=nExists, xx=xExists)
+        else:
+            self.REDIS.set(key, value, nx=nExists, xx=xExists)
+
+    def getURLResponse(self, request):
+        key = request.path.replace('/','-*-') + '-*-'.join([*list(functools.reduce(lambda x, y: x + y, sorted(request.query_params.items())))])
+        return self.REDIS.get(key)
