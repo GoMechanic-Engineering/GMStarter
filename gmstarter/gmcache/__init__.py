2369e4a511ccc25d9e75ad80e3fab3c98c9cb6c2
SydeWynder <prasenjit0625@gmail.com>
2020-07-02 00:30:41 +0530

    redis

--- gmstarter/gmcache/__init__.py
+++ gmstarter/gmcache/__init__.py
@@ -4,7 +4,7 @@ from django.db.models import Model
 from django.apps import apps
 import json
 import functools
-from bson.json_util import dumps
+from bson import json_util
 
 
 class GMCache:
@@ -28,12 +28,12 @@ class GMCache:
 
     def setJson(self, key, value, weeks=0, days=0, hours=0, minutes=0, seconds=0):
         expiry = (weeks*7*24*60*60) + (days*24*60*60) + (hours*60*60) + (hours*60*60) + (minutes*60) + seconds
-        self.REDIS.execute_command('JSON.SET', key, '.', json.dumps(value))
+        self.REDIS.execute_command('JSON.SET', key, '.', json_util.dumps(value))
         if expiry != 0:
             self.REDIS.expire(key, expiry)
 
     def getJson(self, key):
-        return json.loads(self.REDIS.execute_command('JSON.GET', key))
+        return json_util.loads(self.REDIS.execute_command('JSON.GET', key))
 
     def expire(self, key, expiry):
         self.REDIS.expire(key, expiry)
@@ -48,10 +48,11 @@ class GMCache:
         #     self.REDIS.set(key, value, ex=expiry, nx=nExists, xx=xExists)
         # else:
         #     self.REDIS.set(key, value, nx=nExists, xx=xExists)
-        self.REDIS.execute_command('JSON.SET', key, '.', dumps(value))
+        self.REDIS.execute_command('JSON.SET', key, '.', json_util.dumps(value))
         if expiry != 0:
             self.REDIS.expire(key, expiry)
 
     def getURLResponse(self, request):
         key = request.path.replace('/','-*-') + '-*-'.join([*list(functools.reduce(lambda x, y: x + y, sorted(request.query_params.items())))])
-        return self.REDIS.get(key)
+        return json_util.loads(self.REDIS.execute_command('JSON.GET', key))
+        # return self.REDIS.get(key)
