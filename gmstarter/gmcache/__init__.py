6c21d4d4c48fb637ee9cd279e6d186d3e1dd67c1
SydeWynder <prasenjit0625@gmail.com>
2020-07-01 12:10:47 +0530

    redis

--- gmstarter/gmcache/__init__.py
+++ gmstarter/gmcache/__init__.py
@@ -25,8 +25,11 @@ class GMCache:
     def delete(self, *names):
         return self.REDIS.delete(names)
 
-    def setJson(self, key, value):
+    def setJson(self, key, value, weeks=0, days=0, hours=0, minutes=0, seconds=0):
+        expiry = (weeks*7*24*60*60) + (days*24*60*60) + (hours*60*60) + (hours*60*60) + (minutes*60) + seconds
         self.REDIS.execute_command('JSON.SET', key, '.', json.dumps(value))
+        if expiry != 0:
+            self.REDIS.expire(key, expiry)
 
     def getJson(self, key):
         return json.loads(self.REDIS.execute_command('JSON.GET', key))
@@ -40,10 +43,13 @@ class GMCache:
     def setURLResponse(self, request, value=None, weeks=0, days=0, hours=0, minutes=0, seconds=0, nExists=False, xExists=False):
         expiry = (weeks*7*24*60*60) + (days*24*60*60) + (hours*60*60) + (hours*60*60) + (minutes*60) + seconds
         key = request.path.replace('/','-*-') + '-*-'.join([*list(functools.reduce(lambda x, y: x + y, sorted(request.query_params.items())))])
+        # if expiry != 0:
+        #     self.REDIS.set(key, value, ex=expiry, nx=nExists, xx=xExists)
+        # else:
+        #     self.REDIS.set(key, value, nx=nExists, xx=xExists)
+        self.REDIS.execute_command('JSON.SET', key, '.', json.dumps(value))
         if expiry != 0:
-            self.REDIS.set(key, value, ex=expiry, nx=nExists, xx=xExists)
-        else:
-            self.REDIS.set(key, value, nx=nExists, xx=xExists)
+            self.REDIS.expire(key, expiry)
 
     def getURLResponse(self, request):
         key = request.path.replace('/','-*-') + '-*-'.join([*list(functools.reduce(lambda x, y: x + y, sorted(request.query_params.items())))])
