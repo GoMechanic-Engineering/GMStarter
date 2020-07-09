ce982bb7b9359cf9e461daa0adba1a3241f1a70d
SydeWynder <prasenjit0625@gmail.com>
2020-07-09 12:38:53 +0530

    2

--- gmstarter/whatsapp/__init__.py
+++ gmstarter/whatsapp/__init__.py
@@ -8,26 +8,27 @@ import traceback
 p = Producer(settings.KAFKA_CONFIG)
 
 class WhatsappMessage:
+    phone = ""
     templateId = ""
     data = {}
 
-    def __init__(self, templateId, data={}):
-        self.templateId = templateId
-        self.data = data
-        self.send(templateId, data)
+    def __init__(self, phone, templateId, parameterData={}):
+        self.phone = str(phone)
+        self.templateId = str(templateId)
+        self.parameterData = parameterData
+        self.send(phone, templateId, parameterData)
 
-    def send(self, templateId, data):
-        
+    def send(self, phone, templateId, parameterData):
         try:
-            p.produce('whatsapp_message', key=settings.GM_SERVER_NAME, value=json.dumps(self.getFormattedArgs(templateId, data)).encode('utf-8'), callback=None)
+            p.produce('whatsapp_message', key=settings.GM_SERVER_NAME, value=json.dumps(self.getFormattedArgs(phone, templateId, parameterData)).encode('utf-8'), callback=None)
         except Exception as e:
             try:
                 p.produce('whatsapp_message', key=settings.GM_SERVER_NAME, value=json.dumps(self.getFormattedExceptionArgs(str(traceback.format_exc()))).encode('utf-8'), callback=None)
             except Exception as e1:
                 print(str(traceback.format_exc()))
 
-    def getFormattedArgs(self, templateId, data):
-        return {'templateId' : templateId, 'data' : data, 'type' : 'message'}
+    def getFormattedArgs(self, phone, templateId, parameterData):
+        return {'templateId' : templateId, 'parameterData' : parameterData, 'type' : 'message', 'phone' : phone}
 
     def getFormattedExceptionArgs(self, error):
         return {'type' : 'kafka_exception', 'time' : str(datetime.datetime.now()), 'info' : error}
