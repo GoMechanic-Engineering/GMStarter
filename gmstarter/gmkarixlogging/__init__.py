197e190efb21befe4bd40674c8b06d171245a6f7
SydeWynder <prasenjit0625@gmail.com>
2021-03-11 12:09:09 +0530

    1

---
+++ gmstarter/gmkarixlogging/__init__.py
@@ -0,0 +1,38 @@
+import sys
+from confluent_kafka import Producer
+import datetime
+from django.conf import settings
+import json
+import traceback
+
+p = Producer(settings.KAFKA_CONFIG)
+
+class GMLogging:
+    global_name = ""
+
+    def __init__(self, global_name):
+        self.global_name = global_name
+
+    def print(self, *args):
+        print(args)
+        
+        try:
+            p.produce('print_logging', key=self.global_name, value=json.dumps(self.getFormattedArgs(args)).encode('utf-8'), callback=None)
+        except Exception as e:
+            try:
+                p.produce('print_logging', key=self.global_name, value=json.dumps(self.getFormattedExceptionArgs(args)).encode('utf-8'), callback=None)
+            except Exception as e1:
+                print(str(traceback.format_exc()))
+
+    def getFormattedArgs(self, *args):
+        value = {'type' : 'print', 'time' : str(datetime.datetime.now()), 'info' : {}}
+        count = 0
+        for arg in args:
+            value['info']['arg'+str(count)] = str(arg)
+            count += 1
+
+        return value
+
+    def getFormattedExceptionArgs(self, *args):
+        value = {'type' : 'kafka_exception', 'time' : str(datetime.datetime.now()), 'info' : "exception"}
+        return value
